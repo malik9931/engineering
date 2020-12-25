d2262996d9464365b42c537b572d874cf34bc26b
Malek <engmalek.civil@gmail.com>
2020-12-25 21:30:02 +0200

    trying some codes

---
+++ color.js
@@ -0,0 +1,14 @@
+var color = prompt('add one of these color:  red, yellow, blue, brown, orange, green, violet, black');
+if (color === 'red') {
+  color= 'red';
+} else if (color === 'yellow') {
+color = ' yellow';
+} else if (color === 'blue') {
+color = 'blue';
+} else if (color === 'brown') {
+    color = 'brown';
+}
+document.write('<header style="background-color: ' +greeting + '">');
+
+
+// <script src="color.js"></script>
